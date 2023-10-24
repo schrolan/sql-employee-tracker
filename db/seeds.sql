aa90d19b3bbc9bb4cd4db50fe0773f97dd81ab42
schrolan <schrolan000@gmail.com>
2023-10-24 18:05:27 -0500

    I added a scratchpad.sql to run sql queries and added that file to the gitignore.

---
+++ db/seeds.sql
@@ -0,0 +1,15 @@
+INSERT INTO department (name)
+VALUES ("sales")
+
+INSERT INTO department (name)
+VALUES ("engineering")
+
+INSERT INTO department (name)
+VALUES ("it")
+
+INSERT INTO department (name)
+VALUES ("custodial")
+
+INSERT INTO department (name)
+VALUES ("marketing")
+
