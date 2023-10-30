cc8a439c6525e393e229771031284f112e177555
schrolan <schrolan000@gmail.com>
2023-10-30 14:55:13 -0500

    I have the seeds and schema files working now. I had to remember to add semi colons.

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,44 +1,44 @@
 INSERT INTO department (name)
-VALUES ("sales")
+VALUES ("sales");
 
 INSERT INTO department (name)
-VALUES ("engineering")
+VALUES ("engineering");
 
 INSERT INTO department (name)
-VALUES ("it")
+VALUES ("it");
 
 INSERT INTO department (name)
-VALUES ("custodial")
+VALUES ("custodial");
 
 INSERT INTO department (name)
-VALUES ("marketing")
+VALUES ("marketing");
 
 INSERT INTO role (title, salary, department_id)
-VALUES ("Sales lead", 100000, 1)
+VALUES ("Sales lead", 100000, 1);
 
 INSERT INTO role (title, salary, department_id)
-VALUES ("Engineering lead", 90000, 2)
+VALUES ("Engineering lead", 90000, 2);
 
 INSERT INTO role (title, salary, department_id)
-VALUES ("IT lead", 85000, 3)
+VALUES ("IT lead", 85000, 3);
 
 INSERT INTO role (title, salary, department_id)
-VALUES ("Custodial lead", 65000, 4)
+VALUES ("Custodial lead", 65000, 4);
 
 INSERT INTO role (title, salary, department_id)
-VALUES ("Marketing lead", 110000, 5)
+VALUES ("Marketing lead", 110000, 5);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("John", "Johnson", 1, 1)
+VALUES ("John", "Johnson", 1, 1);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Steven", "Stevenson", 2, 2)
+VALUES ("Steven", "Stevenson", 2, 2);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("James", "Jameson", 3, 3)
+VALUES ("James", "Jameson", 3, 3);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("William", "Williamson", 4, 4)
+VALUES ("William", "Williamson", 4, 4);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Tom", "Thompson", 5, 5)
+VALUES ("Tom", "Thompson", 5, 5);
